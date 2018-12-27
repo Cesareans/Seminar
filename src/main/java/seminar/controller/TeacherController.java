7418f0171288565b26866ee8da3cb868eff545b3
Cesare <qc_b89b341f121846769105da09bf21a0dc@git.code.tencent.com>
2018-12-27 13:41:23 +0800

    Catch tail

--- src/main/java/seminar/controller/TeacherController.java
+++ src/main/java/seminar/controller/TeacherController.java
@@ -198,7 +198,6 @@ public class TeacherController {
     public String courseInfo(String courseId, Model model) {
         model.addAttribute("course", seminarService.getCourseByCourseId(courseId).get(0));
         model.addAttribute("strategies", seminarService.getStrategiesByCourseId(courseId));
-
         return "teacher/course/info";
     }
 
